94348a3b73ff83e7575e7b43bb4c9bb42a78841e
Baozi Thrower <weiping.chen@gmail.com>
2018-06-15 14:20:43 -0500

    Update createLiteralUsingForStatement.py

--- createLiteralUsingForStatement.py
+++ createLiteralUsingForStatement.py
@@ -1,12 +1,11 @@
->>> na
-[0, 1, 2, 3, 4, 5]
->>> na1 = [i+1 for i in na]
->>> na1
-[1, 2, 3, 4, 5, 6]
->>> m1 = {'a':1, 'b':2}
->>> m2 = {k:v for k,v in d1.items()}
->>> m2
-{'a': 1, 'b': 2}
->>> m3 = {k:v+10 for k,v in m1.items()}
->>> m3
-{'a': 11, 'b': 12}
+na = [0, 1, 2, 3, 4, 5]
+na1 = [i+1 for i in na]
+print(f'na1={na1}')
+# na1=[1, 2, 3, 4, 5, 6]
+m1 = {'a':1, 'b':2}
+m2 = {k:v for k,v in m1.items()}
+print(f'm2={m2}')
+# m2={'a': 1, 'b': 2}
+m3 = {k:v+10 for k,v in m1.items()}
+print(f'm3={m3}')
+# m3={'a': 11, 'b': 12}
